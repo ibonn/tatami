02ee7a579552b5d6e03f66e70c7f6d9740aabbae
Ibon <ibonescartin@gmail.com>
2025-07-27 11:54:48 +0200

    Update example

--- examples/cli/routers/health.py
+++ examples/cli/routers/health.py
@@ -1,10 +1,36 @@
-from tatami import router, get
+from typing import Annotated
+
+from pydantic import BaseModel, Field
+
+from tatami import get, post, router
+from tatami.param import Query
+
+
+class Post(BaseModel):
+    title: str = Field(description='The post title', examples=['Test post'])
+    author: str = Field(description='Author name', examples=['John Doe'])
+    body: str = Field(description='Post body', examples=['Lorem ipsum, dolor sit amet'])
 
 class Health(router('/health')):
     @get
     def get_health(self):
         """Get app status"""
         return 'OK'
+    
+class Post(router('/post')):
+    @get('/{post_id}')
+    def get_post(self, post_id: int, get_comments: Annotated[bool, Query('show_comments')] = False):
+        result = {'content': 'lorem ipsum', 'id': post_id}
+        if get_comments:
+            result['comments'] = [
+                {'comment_id': 'foo', 'author': 'bar'}
+            ]
+        return result
+    
+    @post
+    def create_post(self, p: Post):
+        return {'success': True, 'post': p}
+    
 
 class NonRouter:
     def get_nothing(self):
