eee35e0f5208cd0baeb1912d457e8b664ec98b4b
Ibon <ibonescartin@gmail.com>
2025-08-01 20:57:14 +0200

    Add copy button to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ release = __version__
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'sphinx_copybutton']
 
 templates_path = ['_templates']
 exclude_patterns = []
